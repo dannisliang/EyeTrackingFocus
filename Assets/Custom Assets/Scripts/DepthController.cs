67e5017344aa542fea2a1368379bca5a65deb11b
benrosen <ben.rosen@neurospire.com>
2015-05-27 16:03:53 -0400

    First Commit
    - Created Project
    - Gitignore
    - Focus on Vector3

---
+++ Assets/Custom Assets/Scripts/DepthController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DepthController : MonoBehaviour {
+
+	Vector3 screenPoint;
+	float distance;
+	Ray ray;
+	RaycastHit hit;
+
+	void Update () {
+		SetScreenPoint (Input.mousePosition);
+		SetDistance ();
+		SetFocusDistance (distance);
+	}
+	
+	void SetScreenPoint (Vector3 newScreenPoint) {
+		screenPoint = newScreenPoint;
+	}
+	
+	void SetDistance () {
+		ray = Camera.main.ScreenPointToRay (screenPoint);
+		
+		if (Physics.Raycast (ray, out hit)) {
+			distance = hit.distance;
+		}
+	}
+	
+	void SetFocusDistance (float distance) {
+		GetComponent<DepthOfFieldScatter>().focalLength = distance;
+	}
+}
